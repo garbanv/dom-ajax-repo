37bb02ca19b0c82422ed1c6c25d1ea1f0353a596
Garbanv <garban.valdeon@gmail.com>
2020-05-16 10:33:40 +0200

    exercise 1 task 1 completed

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,23 @@
 // Write code here to communicate with Github
+
+function getRepos(user) {
+  fetch(`https://api.github.com/users/${user}/repos`)
+    .then(res => res.json())
+    .then(function(response) {
+      const reposCount = document.querySelector("#repos-count");
+      reposCount.innerText = response.length;
+      const repoList = document.querySelector("#repos-list");
+      console.log(response);
+
+      return response.map(function(rep) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.innerText = rep.name;
+        a.setAttribute("href", `${rep.html_url}`);
+        li.appendChild(a);
+        return repoList.appendChild(li);
+      });
+    });
+}
+
+getRepos("garbanv");
